eff4ba3a94b20d4e1a7b21942f3137bdcc931ad2
Andreythesun <tyshkivskiy5@gmail.com>
2020-10-15 15:30:46 +0300

    Remake with functions, only define consonants in word's ends

--- Lab2.1.cpp
+++ Lab2.1.cpp
@@ -1,21 +1,21 @@
 ﻿#include <stdio.h>
-
-char tolower(char x)
+/*char tolower(char x)
 {
 	if ('A' <= x && x <= 'Z')
 	{
-		x = (x + 'a' - 'A');
-		return x;
+		char low;
+		low = 0;
+		low = (x + 30);
+		return(low);
 	}
-	return 0;
+	return 1;
 }
 
 int main()
 {
-	int low;
 	int i = 0, j = 0, k = 0, z = 0;
 	char strin[200], strout[200];
-	char vowels[7] = { 'A','E','Y','U','O','I' };   //massive with vowels
+	char vowels[7] = {'A','E','Y','U','O','I'};   //massive with vowels
 	printf("Input a sentence:\n");   
 	fgets(strin, 199, stdin);
 	for (i = 0; strin[i] != 0; i++) 
@@ -31,7 +31,7 @@ int main()
 		strout[i] = strin[i];   //initialize this letter to output
 		if (strin[i+1] == ' ' && strin[i] != ' ')   //check if this is a last letter of the word adn vowel
 		{
-			for (j = 0; j <= 5 && k != 1; j++)    //check symbol if this is a vowel
+			for (j = 0; j <= 5 && k == 0; j++)    //check symbol if this is a vowel
 			{ 
 				if (strin[i] == vowels[j] || strin[i] == tolower(vowels[j]))
 				{
@@ -54,5 +54,32 @@ int main()
 	printf("\nOnly words which ends with vowel:\n");
 	puts(strout);   //output
 	return 0;
-}
+}*/
+int vowcheck(char x)
+{
+	char vow[] = { 'A','E','Y','U','I','O' };
+	int j;
+	for (j = 0; vow[j] != 0; j++) {
+		if (x == vow[j] || x == vow[j] + 32) {
+			return 1;
+		}
+	}
 
+}
+int main()
+{
+	int i, j = 0;
+	char strin[200], strout[200];
+	printf("Input a sentence:\n");
+	fgets(strin, 199, stdin);
+	for (i = 0; strin[i] != '\n'; i++, j++) {
+		strout[j] = strin[i];
+		if ((strin[i+1] == ' ' || strin[i+1] == '\n' || strin[i+1] == ',' || strin[i+1] == '.') && !(strout[j] == ',' || strout[j] == '.' || strout[j] == ' ')) {
+			if (vowcheck(strout[j]) != 1) {
+				strout[i] = '&';
+			}
+		}
+	}
+	strout[j] = 0;
+	puts(strout);
+}
